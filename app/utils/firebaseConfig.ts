fc712dedacb6f69bffb062543bd36a230efa12b6
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-06-18 00:23:54 +0800

    [AJ] [Added] firestore configuration

--- app/utils/firebaseConfig.ts
+++ app/utils/firebaseConfig.ts
@@ -1,6 +1,7 @@
 // Create firebase config
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const firestore = getFirestore(app);
