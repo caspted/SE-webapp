54f2f238bdab07afdf42bd0944f865947c6c48b7
wends05 <cxnner05@gmail.com>
2024-06-27 23:15:07 +0800

    [Wends] [Add] Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react";
+import next from "@/public/next.svg";
+
+const Header = () => {
+
+  return (
+    <main className={"flex fixed p-5 justify-between w-full"}>
+      <Image src={next} width={50} height={50} alt="bg image" />
+      <header className="md:hidden block">
+        <button>
+          Menu
+        </button>
+      </header>
+      <header className="hidden md:flex"></header>
+    </main>
+  );
+};
+
+export default Header;
