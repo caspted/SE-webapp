0427eca76abb29da87100861661e0e6a356efe00
wends05 <cxnner05@gmail.com>
2024-06-29 22:22:22 +0800

    [Wends] [Layout] landing page

---
+++ app/(landing page)/page.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Events from "./Events";
+import Projects from "./Projects";
+import Sponsors from "./Sponsors";
+import Testimonials from "./Testimonials";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <Header />
+      <Testimonials />
+      <Events />
+      <Projects />
+      <Sponsors />
+    </main>
+  );
+}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import next from "@/public/next.svg";
 
 const Header = () => {
-
   return (
     <main className={"flex fixed p-5 justify-between w-full"}>
       <Image src={next} width={50} height={50} alt="bg image" />
