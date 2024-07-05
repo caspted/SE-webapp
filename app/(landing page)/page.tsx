56b693249912143afb7b5418a1e14579f94ed38a
wends05 <cxnner05@gmail.com>
2024-07-05 23:29:24 +0800

    [Wends] [Setup] landing page ( (landingpage)/page.tsx )

--- app/(landing page)/page.tsx
+++ app/(landing page)/page.tsx
@@ -1,6 +1,7 @@
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Events from "./Events";
+import Hero from "./Hero";
 import Projects from "./Projects";
 import Sponsors from "./Sponsors";
 import Testimonials from "./Testimonials";
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <Header />
+      <Hero />
       <Testimonials />
       <Events />
       <Projects />
