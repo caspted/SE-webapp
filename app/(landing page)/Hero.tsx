5198ae3e528df903348c51cc21a760254f95a0fd
dddddrrrrreeeeiiiiiz <157622562+dddddrrrrreeeeiiiiiz@users.noreply.github.com>
2024-08-04 00:51:25 +0800

    [Paul] [add] designs on hero page

--- app/(landing page)/Hero.tsx
+++ app/(landing page)/Hero.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import BackgroundImage from "@/public/HeroBg.png";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center'>Hero</div>
-  )
-}
+    <main>
+      <div className="h-28 bg-gradient-to-t from-white opacity-20" />
+      <div className="relative flex max-h-[120vh] min-h-[55vh] w-full flex-col justify-between overflow-hidden p-4 text-center md:min-h-screen">
+        <Image
+          src={BackgroundImage}
+          alt="random-image"
+          fill
+          className="-z-10 h-full object-cover"
+          placeholder="blur"
+        />
 
-export default Hero
+        <h1 className="font-medium"> TRANSFORM YOUR IDEAS INTO A REALITY</h1>
+
+        <p>
+          Are you ready to bring your ideas to life and revolutionize the world
+          with technology? Enroll in our Software Engineering program today and
+          unlock endless possibilities!
+        </p>
+
+        <div>
+          <button>INQUIRE</button>
+          <button>VIEW OUR CURRICULUM</button>
+        </div>
+
+        <div className="h-28 bg-gradient-to-b from-white opacity-20" />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
