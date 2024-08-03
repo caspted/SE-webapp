9245720a4a71dca0662226ae1e28b221b4d880a5
dddddrrrrreeeeiiiiiz <157622562+dddddrrrrreeeeiiiiiz@users.noreply.github.com>
2024-08-04 02:24:46 +0800

    [Paul] [add] small screen of hero page

--- app/(landing page)/Hero.tsx
+++ app/(landing page)/Hero.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import BackgroundImage from "@/public/HeroBg.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <main>
-      <div className="h-28 bg-gradient-to-t from-white opacity-20" />
-      <div className="relative flex max-h-[120vh] min-h-[55vh] w-full flex-col justify-between overflow-hidden p-4 text-center md:min-h-screen">
+      <div className="relative flex max-h-[120vh] min-h-[55vh] w-full flex-col justify-center overflow-hidden p-4 text-center md:min-h-screen items-center space-y-2">
         <Image
           src={BackgroundImage}
           alt="random-image"
@@ -15,20 +15,21 @@ const Hero = () => {
           placeholder="blur"
         />
 
-        <h1 className="font-medium"> TRANSFORM YOUR IDEAS INTO A REALITY</h1>
+        <p className="font-medium text-[20px]"> TRANSFORM YOUR IDEAS INTO A REALITY</p>
 
-        <p>
+        <p className="font-normal text-[14px] ">
           Are you ready to bring your ideas to life and revolutionize the world
           with technology? Enroll in our Software Engineering program today and
           unlock endless possibilities!
         </p>
 
-        <div>
+        <div className="flex-col flex gap-1 space-y-1 pt-2">
+          <Link href={'/'} className="font-semibold text-[10px] text-center">
           <button>INQUIRE</button>
-          <button>VIEW OUR CURRICULUM</button>
+          </Link>
+          <Link href={'/'} className="text-[#FE9901] text-[10px] font-semibold underline underline-offset-2 ">VIEW OUR CURRICULUM</Link>
         </div>
 
-        <div className="h-28 bg-gradient-to-b from-white opacity-20" />
       </div>
     </main>
   );
