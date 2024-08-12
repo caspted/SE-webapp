39873aa1f1f2db81b5f7063fe7651cd764d9b7e0
dddddrrrrreeeeiiiiiz <157622562+dddddrrrrreeeeiiiiiz@users.noreply.github.com>
2024-08-13 04:37:57 +0800

    [Paul] [add] navbar(unfinished in small screens)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,49 @@
 import Image from "next/image";
 import React from "react";
-import next from "@/public/next.svg";
+import Logo from "@/public/SeLogo.png";
+import Hamburger from "@/public/Hamburger.svg";
+import Link from "next/link";
+import DropDown from "@/public/dropdown.svg";
+import { useState } from "react";
 
 const Header = () => {
   return (
-    <main className={"fixed z-50 flex w-full justify-between p-5"}>
-      <Image src={next} width={50} height={50} alt="bg image" />
-      <header className="block md:hidden">
-        <button>Menu</button>
-      </header>
-      <header className="hidden md:flex"></header>
-    </main>
+    <nav className="fixed top-0 z-[20] flex h-[100px] w-full items-center justify-between bg-transparent px-5 text-white md:items-center md:bg-primary-900 md:px-10">
+      <div className="relative max-w-10 md:max-w-12">
+        <Image src={Logo} width={50} height={50} alt="Logo" />
+      </div>
+      <div className="hidden md:flex">
+        <Link href="/" className="mx-2 hover:bg-indigo-800 rounded-lg p-2">
+          About
+        </Link>
+        <Link href="/" className="mx-2  hover:bg-indigo-800 rounded-lg p-2">
+          News
+        </Link>
+
+        <Link href="/" className="mx-2 hover:bg-indigo-800 rounded-lg gap-2 p-2 flex justify-center
+        ">
+          People
+          <Image src={DropDown} width={15} height={15} alt={"arrow"} className="relative text-white"/>
+        </Link>
+
+        <Link href="/" className="mx-2  hover:bg-indigo-800 rounded-lg p-2">
+          Events
+        </Link>
+        <Link href="/" className="mx-2  hover:bg-indigo-800 rounded-lg p-2">
+          Contacts
+        </Link>
+        <Link href="/" className="mx-2  hover:bg-indigo-800 rounded-lg p-2">
+          Curriculum
+        </Link>
+      </div>
+      <div className="flex w-full max-w-[50px] justify-between">
+        <button className="border-none bg-transparent md:hidden">
+          <Image src={Hamburger} width={50} height={50} alt="Menu" />
+        </button>
+      </div>
+    </nav>
   );
 };
 
 export default Header;
+
