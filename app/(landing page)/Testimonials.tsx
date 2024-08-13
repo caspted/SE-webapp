7d451f48dffd07ecaaff4f7f7005066ea7d751bd
wends05 <cxnner05@gmail.com>
2024-08-14 03:52:18 +0800

    [Wends] [fixed] visual change of height in testimonials

--- app/(landing page)/Testimonials.tsx
+++ app/(landing page)/Testimonials.tsx
@@ -44,14 +44,13 @@ const Testimonials = () => {
   return (
     <div className="bg-primary-900 p-4 text-white md:p-8">
       <h1 className="mb-6 text-4xl font-semibold md:mb-6"> SE Testimonials</h1>
-      <div className="relative flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
-          <div className="relative rounded-3xl">
+      <div className="relative flex h-[400px] justify-center md:justify-around">
+        <div className="flex h-[60vh] max-h-[400px] w-[60vh] max-w-[400px] flex-col">
+          <div className="relative flex h-full w-full rounded-3xl">
             <Image
               src={testimonials[currentIndex].image}
               alt="Person"
-              width={288}
-              height={288}
+              fill
               className="w-full rounded-3xl object-fill"
             />
             <button
@@ -66,7 +65,7 @@ const Testimonials = () => {
             >
               <SlArrowRight />
             </button>
-            <div className="mt-2 flex justify-center">
+            <div className="absolute -bottom-4 left-1/2 flex -translate-x-1/2 justify-center">
               {testimonials.map((_, index) => (
                 <div
                   key={index}
@@ -76,18 +75,17 @@ const Testimonials = () => {
             </div>
           </div>
         </div>
-        <div className="hidden flex-col md:mb-[50px] md:block lg:mb-[100px]">
-          <FaQuoteLeft
-            className="md:mb-[10px] md:ml-[20px] lg:mb-[0px] lg:ml-[63px]"
-            size={40}
-          />
-          <p className="z-10 mb-8 ml-[70px] w-[292px] text-base font-normal lg:ml-[148px] lg:mt-[20px] lg:w-[220px] xl:w-[366px]">
-            {testimonials[currentIndex].text}
-          </p>
+        <div className="h-full">
+          <div className="hidden flex-col md:block">
+            <FaQuoteLeft className="md:mb-[10px] lg:mb-[0px]" size={40} />
+            <p className="z-10 mb-8 w-[292px] lg:mt-[20px] lg:w-[220px] xl:w-[366px]">
+              {testimonials[currentIndex].text}
+            </p>
+          </div>
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="sm:h-26 -mt-6 h-[100px] w-[288px] rounded-2xl bg-primary-300 text-center sm:w-72 md:-mt-16 md:h-32 md:w-[704px] md:justify-start lg:-mt-16 lg:w-[958px] xl:-mt-16 xl:w-[1062px]">
+        <div className="sm:h-26 -mt-6 h-[100px] w-[288px] rounded-2xl bg-primary-300 pt-3 text-center sm:w-72 md:-mt-16 md:h-32 md:w-[704px] md:justify-start lg:-mt-16 lg:w-[958px] xl:-mt-16 xl:w-[1062px]">
           <h2 className="mt-8 text-xl font-semibold md:ml-[422px] md:mt-[35px] md:text-left lg:ml-[613px] xl:ml-[541px]">
             {testimonials[currentIndex].name}
           </h2>
